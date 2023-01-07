b9d154b04431355062397858d8fb24539a739516
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-07 20:33:54 -0300

    add .env and db connections

--- database/db.ts
+++ database/db.ts
@@ -15,27 +15,27 @@ const mongooConnection = {
 
 export const connect = async () => {
     if (mongooConnection.isConnected) {
-        console.log('Ya estamos conectados')
+        console.log('Ya estabamos conectados')
         return
     }
 
     if (mongoose.connections.length > 0) {
         mongooConnection.isConnected = mongoose.connections[0].readyState
         if (mongooConnection.isConnected === 1) {
-            console.log('Usnado conexion anterior')
+            console.log('Usando conexion anterior')
             return
         }
         await mongoose.disconnect()
     }
 
-    await mongoose.connect('....')
+    await mongoose.connect(process.env.MONGO_URL || '')
     mongooConnection.isConnected = 1
-    console.log('Conectando a MongoDB: ', '....')
+    console.log('Conectando a MongoDB: ', process.env.MONGO_URL)
 }
 
 export const disconnect = async () => {
 
-    if (mongooConnection.isConnected !== 0) return
+    if (mongooConnection.isConnected === 0) return
 
     await mongoose.disconnect()
     console.log('Desconectado de MongoDB')
