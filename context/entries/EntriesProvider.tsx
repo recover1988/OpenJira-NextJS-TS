b9a6998b03b536ef9b52ad8a6cecf9706b10da29
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-07 23:35:10 -0300

    using GET entries in context>entries

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,7 +1,8 @@
-import { FC, useReducer } from 'react'
+import { FC, useEffect, useReducer } from 'react'
 import { v4 as uuidv4 } from 'uuid';
 import { EntriesContext, entriesReducer } from './'
 import { Entry } from '../../interfaces'
+import { entriesApi } from '../../api';
 
 export interface EntriesState {
     entries: Entry[]
@@ -36,6 +37,16 @@ export const EntriesProvider: FC<Props> = ({ children }) => {
     const updateEntry = (entry: Entry) => {
         dispatch({ type: '[Entry] Entry-Updated', payload: entry })
     }
+
+    const refreshEntries = async () => {
+        const { data } = await entriesApi.get<Entry[]>('/entries')
+        dispatch({ type: '[Entry] Refresh-Data', payload: data })
+    }
+
+    useEffect(() => {
+        refreshEntries()
+    }, [])
+
 
     return (
         <EntriesContext.Provider value={{
