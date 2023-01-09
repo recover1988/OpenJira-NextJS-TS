24bc5ab05e6a2bd75e011c7d33fec790872fa7fd
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-08 22:40:00 -0300

    adding endpoint PUT by id Entry

---
+++ pages/api/entries/[id].ts
@@ -0,0 +1,42 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import mongoose from 'mongoose';
+import { db } from '../../../database';
+import { Entry, IEntry } from '../../../models';
+
+type Data =
+    | { message: string }
+    | IEntry
+
+export default function (req: NextApiRequest, res: NextApiResponse<Data>) {
+    const { id } = req.query // siempre son string el query
+    if (!mongoose.isValidObjectId(id)) {
+        return res.status(400).json({ message: 'El id no es valido ' + id })
+    }
+
+    switch (req.method) {
+        case 'PUT':
+            return updateEntry(req, res)
+
+        default:
+            return res.status(400).json({ message: 'Metodo no existe' })
+    }
+}
+
+const updateEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+    const { id } = req.query
+    await db.connect()
+    const entryToUpdate = await Entry.findById(id)
+    if (!entryToUpdate) {
+        await db.disconnect()
+        return res.status(400).json({ message: 'No ha entradas conn ese ID: ' + id })
+    }
+
+    const {
+        description = entryToUpdate.description,
+        status = entryToUpdate.status
+    } = req.body
+    const updatedEntry = await Entry.findByIdAndUpdate(id, { description, status }, { runValidators: true, new: true })
+
+    res.status(200).json(updatedEntry!)
+
+}
