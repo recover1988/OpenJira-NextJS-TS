b90f3c5fba61c47022f0cced56bf157dbc216d07
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-08 23:33:33 -0300

    adding method GET to [id].ts

--- pages/api/entries/[id].ts
+++ pages/api/entries/[id].ts
@@ -17,11 +17,14 @@ export default function (req: NextApiRequest, res: NextApiResponse<Data>) {
     switch (req.method) {
         case 'PUT':
             return updateEntry(req, res)
+        case 'GET':
+            return getEntry(req, res)
 
         default:
             return res.status(400).json({ message: 'Metodo no existe' })
     }
 }
+
 
 const updateEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
     const { id } = req.query
@@ -37,22 +40,41 @@ const updateEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
         status = entryToUpdate.status
     } = req.body
 
-try {
-    const updatedEntry = await Entry.findByIdAndUpdate(id, { description, status }, { runValidators: true, new: true })
-    await db.disconnect()
-    res.status(200).json(updatedEntry!)
-    
-} catch (error) {
-    console.log({error})
-    await db.disconnect()
-    res.status(400).json({message:JSON.stringify(error)})
-    // si conoce el error podemos ser mas especificos podemos poner en message: error.errors.status.message y esto nos daria un mensaje
-    // pero abria que ver para cada caso si es posible
-}
+    try {
+        const updatedEntry = await Entry.findByIdAndUpdate(id, { description, status }, { runValidators: true, new: true })
+        await db.disconnect()
+        res.status(200).json(updatedEntry!)
+
+    } catch (error) {
+        console.log({ error })
+        await db.disconnect()
+        res.status(400).json({ message: JSON.stringify(error) })
+        // si conoce el error podemos ser mas especificos podemos poner en message: error.errors.status.message y esto nos daria un mensaje
+        // pero abria que ver para cada caso si es posible
+    }
 
     // entryToUpdate.description=description
     // entryToUpdate.status=status
     // await entryToUpdate.save()
 
+
+}
+const getEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+    const { id } = req.query
+    await db.connect()
+    const entryInDB = await Entry.findById(id)
+    await db.disconnect()
+    if (!entryInDB) {
+        return res.status(400).json({ message: 'No hay entrada con ese ID: ' + id })
+    }
+    return res.status(200).json(entryInDB)
+    // try {
+    //     await db.disconnect()
+    //     res.status(200).json(entryInDB!)
+    // } catch (error) {
+    //     console.log({ error })
+    //     await db.disconnect()
+    //     res.status(400).json({ message: JSON.stringify(error) })
+    // }
 
 }
