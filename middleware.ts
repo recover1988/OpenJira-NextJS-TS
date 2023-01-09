22cddb2ddf9a270e0dea07a68ff2a8ecef78a40b
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-09 17:01:58 -0300

    adding bad-request

--- middleware.ts
+++ middleware.ts
@@ -3,9 +3,19 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-
+export function middleware(req: NextRequest) {
     //console.log({ request: request.nextUrl }) // informacion detallada de las peticiones
+    if (req.nextUrl.pathname.startsWith('/api/entries/')) {
+        const id = req.nextUrl.pathname.replace('/api/entries/', '')
+        const checkMongoIDRegExp = new RegExp("^[0-9a-fA-F]{24}$");
+        
+        if (!checkMongoIDRegExp.test(id)) {
+            const url = req.nextUrl.clone();
+            url.pathname = '/api/bad-request'
+            url.search=`?message=${id} is not a valid MongoID`
+            return NextResponse.rewrite(url)
+        }
+    }
 
     return NextResponse.next() // el .next() indica que no haga nada solo siga
 }
